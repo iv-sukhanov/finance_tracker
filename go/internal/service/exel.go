b010872e172de59586e36b0c8bd042cd6f94b30c
Ivan Sukhanov <ivan261104@gmail.com>
2025-03-16 22:15:24 +0200

    file names are now unique

--- go/internal/service/exel.go
+++ go/internal/service/exel.go
@@ -1,40 +1,23 @@
 package service
 
 import (
+	"crypto/sha1"
 	"fmt"
+	"time"
 
 	ftracker "github.com/iv-sukhanov/finance_tracker/internal"
 	"github.com/xuri/excelize/v2"
 )
 
 const (
-	formatOut = "Monday, 02 Jan, 15:04"
+	formatOut      = "Monday, 02 Jan, 15:04"
+	amountLen      = 10
+	descriptionLen = 30
+	timeLen        = 25
 )
 
-type ExelService struct {
-}
-
-func NewExelService() *ExelService {
-	return &ExelService{}
-}
-
-func (s *ExelService) CreateExelFromRecords(username string, recods []ftracker.SpendingRecord) error {
-
-	f := excelize.NewFile()
-	defer func() {
-		if err := f.Close(); err != nil {
-			panic(err) //TODO: handle error
-		}
-	}()
-
-	index, err := f.NewSheet(username)
-	if err != nil {
-		return fmt.Errorf("CreateExelFromRecords: %w", err)
-	}
-	f.DeleteSheet("Sheet1")
-	f.SetActiveSheet(index)
-
-	headerStyle, err := f.NewStyle(&excelize.Style{
+var (
+	headerStyle = excelize.Style{
 		Font: &excelize.Font{
 			Bold:  true,
 			Size:  12,
@@ -51,21 +34,52 @@ func (s *ExelService) CreateExelFromRecords(username string, recods []ftracker.S
 			{Type: "bottom", Color: "000000", Style: 1},
 			{Type: "right", Color: "000000", Style: 1},
 		},
-	})
-	if err != nil {
-		return fmt.Errorf("CreateExelFromRecords: %w", err)
 	}
 
-	dataStyle, err := f.NewStyle(&excelize.Style{
+	dataStyle = excelize.Style{
 		Border: []excelize.Border{
 			{Type: "left", Color: "000000", Style: 1},
 			{Type: "top", Color: "000000", Style: 1},
 			{Type: "bottom", Color: "000000", Style: 1},
 			{Type: "right", Color: "000000", Style: 1},
 		},
-	})
+	}
+)
+
+type ExelService struct {
+}
+
+func NewExelService() *ExelService {
+	return &ExelService{}
+}
+
+func (s *ExelService) CreateExelFromRecords(username string, recods []ftracker.SpendingRecord) (outputError error) {
+
+	f := excelize.NewFile()
+	defer func() {
+		if err := f.Close(); err != nil {
+			outputError = fmt.Errorf("CreateExelFromRecords: %w %w", err, outputError)
+		}
+	}()
+
+	index, err := f.NewSheet(username)
 	if err != nil {
-		return fmt.Errorf("CreateExelFromRecords: %w", err)
+		outputError = fmt.Errorf("CreateExelFromRecords: %w", err)
+		return outputError
+	}
+	f.DeleteSheet("Sheet1")
+	f.SetActiveSheet(index)
+
+	headerStyle, err := f.NewStyle(&headerStyle)
+	if err != nil {
+		outputError = fmt.Errorf("CreateExelFromRecords: %w", err)
+		return outputError
+	}
+
+	dataStyle, err := f.NewStyle(&dataStyle)
+	if err != nil {
+		outputError = fmt.Errorf("CreateExelFromRecords: %w", err)
+		return outputError
 	}
 
 	f.SetSheetRow(username, "A1", &[]any{"Amount", "Description", "Created At"})
@@ -82,12 +96,18 @@ func (s *ExelService) CreateExelFromRecords(username string, recods []ftracker.S
 		f.SetCellStyle(username, start, end, dataStyle)
 	}
 
-	f.SetColWidth(username, "A", "A", 8)
-	f.SetColWidth(username, "B", "B", 30)
-	f.SetColWidth(username, "C", "C", 25)
+	f.SetColWidth(username, "A", "A", amountLen)
+	f.SetColWidth(username, "B", "B", descriptionLen)
+	f.SetColWidth(username, "C", "C", timeLen)
 
-	if err := f.SaveAs(fmt.Sprintf("%s.xlsx", username)); err != nil {
-		return fmt.Errorf("CreateExelFromRecords: %w", err)
+	secondsOffset := time.Now().Nanosecond()
+	hash := sha1.New()
+	hash.Write(fmt.Appendf(nil, "%s%d", username, secondsOffset))
+	filename := fmt.Sprintf("%x.xlsx", hash.Sum(nil))
+
+	if err := f.SaveAs(filename); err != nil {
+		outputError = fmt.Errorf("CreateExelFromRecords: %w", err)
+		return outputError
 	}
 
 	return nil
